4358a8763c7a120395ef857ae262b71eb4a17e13
Edson Maia <edsonmaia2005@hotmail.com>
2023-01-03 22:24:32 -0300

    Aula 10
    Códigos da aula 10

---
+++ src/app/controllers/SelecaoController.js
@@ -0,0 +1,69 @@
+import conexao from '../database/conexao.js'
+
+class SelecaoController {
+   
+    index(req, res) {
+        const sql = "SELECT * FROM selecoes;"
+        conexao.query(sql, (erro, resultado) => {
+            if(erro) {
+                res.status(404).json({ 'erro': erro})
+            } else {
+                res.status(200).json(resultado)
+            }
+        })
+    }
+
+    show(req, res) {
+        const id = req.params.id
+        const sql = "SELECT * FROM selecoes WHERE id=?;"
+        conexao.query(sql, id, (erro, resultado) => {
+            const linha = resultado[0]
+            if(erro) {
+                res.status(404).json({ 'erro': erro})
+            } else {
+                res.status(200).json(linha)
+            }
+        })
+    }
+
+    store(req, res) {
+        const selecao = req.body
+        const sql = "INSERT INTO selecoes SET ?"
+        conexao.query(sql, selecao, (erro, resultado) => {
+            if(erro) {
+                res.status(404).json({ 'erro': erro})
+            } else {
+                res.status(201).json(resultado)
+            }
+        })
+    }
+
+    update(req, res) {
+        const id = req.params.id
+        const selecao = req.body
+        const sql = "UPDATE selecoes SET ? WHERE id=?;"
+        conexao.query(sql, [selecao, id], (erro, resultado) => {
+            if(erro) {
+                res.status(404).json({ 'erro': erro})
+            } else {
+                res.status(200).json(resultado)
+            }
+        })
+    }
+
+    delete(req, res) {
+        const id = req.params.id
+        const sql = "DELETE FROM selecoes WHERE id=?;"
+        conexao.query(sql, id, (erro, resultado) => {
+            if(erro) {
+                res.status(404).json({ 'erro': erro})
+            } else {
+                res.status(200).json(resultado)
+            }
+        })
+    }
+
+}
+
+// padrão Singleton
+export default new SelecaoController()
